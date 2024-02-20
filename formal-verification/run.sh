dd646140443f494a3b442ad1768b3cfbeef45bbb
Rangel Stoilov <rangelstoilov@gmail.com>
2024-02-20 17:46:13 +0200

    fix: use run.sh to start tests

--- formal-verification/run.sh
+++ formal-verification/run.sh
@@ -8,19 +8,8 @@ echo "Running make record"
 # get any args passed to this script
 args=("$@")
 
-# check if the arg is only one
-if [ $# -eq 1 ]; then
-  # if the arg is only one, then it is the file name
-  FILE_NAME=$1
-  certoraRun  "$SCRIPT_DIR"/certora/conf/"$FILE_NAME".conf
-else 
-  # if we have more than one arg, then we have an invalid number of args
-  # error "Invalid number of arguments"
-  if [ $# -gt 1 ]; then
-	echo "Invalid number of arguments"
-	exit 1
-  else
-    # if we have no args, then we run the default conf file
-	certoraRun  "$SCRIPT_DIR"/certora/conf/default.conf
-  fi	
-fi
+# if the arg is only one, then it is the file name
+FILE_NAME=$1
+# append all the rest of args 
+REST_OF_ARGS=${args[@]:1}
+certoraRun  "$SCRIPT_DIR"/certora/conf/"$FILE_NAME".conf $REST_OF_ARGS
