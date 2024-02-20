0d02f67165f73a7a479ba1d9ee50e08970f0fba0
Rangel Stoilov <rangelstoilov@gmail.com>
2024-02-20 17:36:34 +0200

    feat: add github actions to run certora + add args for run.sh to pass a different conf file

--- formal-verification/run.sh
+++ formal-verification/run.sh
@@ -5,4 +5,22 @@ echo "Running make record"
 
 "$SCRIPT_DIR"/make-patch.sh
 
-certoraRun  "$SCRIPT_DIR"/certora/conf/default.conf
+# get any args passed to this script
+args=("$@")
+
+# check if the arg is only one
+if [ $# -eq 1 ]; then
+  # if the arg is only one, then it is the file name
+  FILE_NAME=$1
+  certoraRun  "$SCRIPT_DIR"/certora/conf/"$FILE_NAME".conf
+else 
+  # if we have more than one arg, then we have an invalid number of args
+  # error "Invalid number of arguments"
+  if [ $# -gt 1 ]; then
+	echo "Invalid number of arguments"
+	exit 1
+  else
+    # if we have no args, then we run the default conf file
+	certoraRun  "$SCRIPT_DIR"/certora/conf/default.conf
+  fi	
+fi
